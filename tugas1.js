d5e0aa18f55c5cf9943f763bbfc5b8383cdd4179
DanangFirmantara <danangfirmantara837@gmail.com>
2022-01-22 17:20:15 +0700

    enter

--- tugas1.js
+++ tugas1.js
@@ -17,6 +17,23 @@ rl.question("Input a word ", function(word) {
     } else {
         console.log (`${input} is not polindrom word`)
     }
+
     rl.close()
 })
 
+// function polindrom(data) {
+//     const input = data
+//     let x = input.length - 1
+//     result = ""
+//     while (x>= 0){
+//         result += input[x]
+//         x--
+//     }
+//     if (result == input){
+//         console.log(`${input} is polindrom word`)
+//     } else {
+//         console.log (`${input} is not polindrom word`)
+//     }
+// }
+
+// polindrom("danang")
--- tugas2.js
+++ tugas2.js
@@ -1,11 +1,37 @@
-const readline = require('readline')
-const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout
-})
 
-rl.question("input a sentence ", function(sentence) {
-    const input = sentence
+// //input from user
+// const readline = require('readline')
+// const rl = readline.createInterface({
+//     input : process.stdin,
+//     output : process.stdout
+// })
+
+// rl.question("input a sentence ", function(sentence) {
+//     const input = sentence
+//     temp = input.length - 1
+//     let x = 0
+//     let found = ""
+//     let res = ""
+//     while (x <= temp){
+//         if (input[x] == ' '){
+//             res = found + ' ' + res
+//             found = ''
+//         } else {
+//             found += input[x]
+//         }
+//         if (x == temp){
+//             res = found + ' ' + res
+//         }
+//         x++
+//     }
+//     console.log(res)
+//     rl.close()
+// })
+
+
+//input has initialization
+function reseredWord(data) {
+    const input = data
     temp = input.length - 1
     let x = 0
     let found = ""
@@ -23,5 +49,6 @@ rl.question("input a sentence ", function(sentence) {
         x++
     }
     console.log(res)
-    rl.close()
-})
+}
+
+reseredWord("Aku Bukan Boneka")
